38be04e79eda44086370349b641a385ce75f46e9
Utsav <utsavbajra619@gmail.com>
2024-10-15 15:12:56 -0400

    add weather API key

--- utils/weatherData.js
+++ utils/weatherData.js
@@ -1,2 +1,12 @@
 const request = require("request");
 
+const openWeatherMap ={
+
+    BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+    SECRET_KEY: "47a6676f7d0dac03dcf6a96aa1e8a074"
+}
+
+
+const weatherData = (address, callback) => {
+    const url = openWeatherMap.BASE_URL + encodeURIComponent(address)+ "&APPID=" + openWeatherMap.SECRET_KEY;
+};
