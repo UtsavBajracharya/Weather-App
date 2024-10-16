a049652ec158af2d2fd6fcc0d861348d7039b4ff
Utsav <utsavbajra619@gmail.com>
2024-10-16 10:05:25 -0400

    add weather data functions with error handling messages

--- src/app.js
+++ src/app.js
@@ -5,17 +5,31 @@ const path = require("path");
 
 const app = express();
 
+const weatherData = require("../utils/weatherData")
+
 const port = process.env.PORT || 3000;
 
 app.get("/" , (req, res) => {
     res.send("Hello this is weather app")
 });
 
+//Fetch the weather details
 app.get("/weather", (req, res) => {
     if(!req.query.address) {
         return res.send("Address is required")
     }
+    weatherData(req.query.address, (error, result) => {
+        if(error) {
+            return res.send(error);
+        }
+
+        res.send(result);
+    });
 });
+
+app.get("*", (req, res) => {
+    res.send("This route doesn't exists");
+})
 
 
 app.listen(port, () => {
--- utils/weatherData.js
+++ utils/weatherData.js
@@ -3,10 +3,21 @@ const request = require("request");
 const openWeatherMap ={
 
     BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-    SECRET_KEY: "47a6676f7d0dac03dcf6a96aa1e8a074"
-}
+    SECRET_KEY: "47a6676f7d0dac03dcf6a96aa1e8a074",
+};
 
 
 const weatherData = (address, callback) => {
     const url = openWeatherMap.BASE_URL + encodeURIComponent(address)+ "&APPID=" + openWeatherMap.SECRET_KEY;
+    console.log(url);
+
+    request({url, json: true}, (error, data) => {
+       if(error) {
+        callback(true, "Unable to fetch data, please try again" + error);
+       } 
+       callback(false, data?.body);
+    });
+
 };
+
+module.exports = weatherData;
